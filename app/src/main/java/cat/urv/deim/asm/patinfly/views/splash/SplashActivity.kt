9e989a467565db21b0b417aa14854cddaf312bff
claudiatombascoll <claudia.tombas@estudiants.urv.cat>
2023-03-20 09:36:35 +0100

    Splash acabat

--- app/src/main/java/cat/urv/deim/asm/patinfly/views/splash/SplashActivity.kt
+++ app/src/main/java/cat/urv/deim/asm/patinfly/views/splash/SplashActivity.kt
@@ -2,28 +2,29 @@ package cat.urv.deim.asm.patinfly.views.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cat.urv.deim.asm.patinfly.R
+import android.view.WindowManager
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 
 
 class SplashActivity : AppCompatActivity() {
-
-    companion object {
-        const val TIME_SPLASH_SCREEN = 2000L
-        private val TAG = SplashActivity::class.java.simpleName
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        exitSplashScreen()
-    }
 
-    private fun exitSplashScreen() {
-        val handler = Handler()
-        handler.postDelayed({
-            launchActivity<SplashActivity> { }
-            overridePendingTransition(android.R.anim.fade_in, R.anim.zoom_out)
-            this.finish()
-        }, TIME_SPLASH_SCREEN)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, SplashActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
