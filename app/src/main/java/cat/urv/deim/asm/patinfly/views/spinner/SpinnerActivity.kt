350bb9f77118c0c69b84dea2b495fe6a3bd30f5d
Sara Azzouz Barrera <sara.azzouz@estudiants.urv.cat>
2023-03-21 12:58:08 +0100

    Spinner terminado

--- app/src/main/java/cat/urv/deim/asm/patinfly/views/spinner/SpinnerActivity.kt
+++ app/src/main/java/cat/urv/deim/asm/patinfly/views/spinner/SpinnerActivity.kt
@@ -2,6 +2,9 @@ package cat.urv.deim.asm.patinfly.views.spinner
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import cat.urv.deim.asm.patinfly.R
@@ -10,15 +13,25 @@ import cat.urv.deim.asm.patinfly.views.signup.SignupInteraction
 import cat.urv.deim.asm.patinfly.views.signup.SignupPresenter
 import java.util.*
 
-class SpinnerActivity: AppCompatActivity() {
+class SpinnerActivity: AppCompatActivity(), AdapterView.OnItemSelectedListener{
     private lateinit var mBinding: ActivitySignUpBinding
-        override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var countries: ArrayAdapter<String>
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-            mBinding= ActivitySignUpBinding.inflate(layoutInflater)
-            setContentView(mBinding.root)
-            val countries= ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item)
-            countries.addAll(Arrays.asList("American", "Belgium", "Canadian",
-                "German", "French", "Mexican", "Spanish"))
-            mBinding.Nacionality.adapter= countries
-        }
+        mBinding= ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        val countries= ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item)
+        countries.addAll(Arrays.asList("American", "Belgium", "Canadian",
+            "German", "French", "Mexican", "Spanish"))
+        mBinding.Nacionality.onItemSelectedListener= this
+        mBinding.Nacionality.adapter= countries
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+        var nSelected= countries.getItem(position)
+        mBinding.tvSelected.text= position.toString()
+    }
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+
+    }
 }
