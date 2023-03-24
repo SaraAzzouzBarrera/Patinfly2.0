12f079d911409d50db682febdf1258ab34004693
claudiatombascoll <claudia.tombas@estudiants.urv.cat>
2023-03-24 12:11:46 +0100

    falta foto i text tutorial

--- app/src/main/java/cat/urv/deim/asm/patinfly/views/tutorial/TutorialActivity.kt
+++ app/src/main/java/cat/urv/deim/asm/patinfly/views/tutorial/TutorialActivity.kt
@@ -1,42 +1,43 @@
 package cat.urv.deim.asm.patinfly.views.tutorial
 
-
-import android.os.Build.VERSION_CODES.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
+import cat.urv.deim.asm.patinfly.R
 import me.relex.circleindicator.CircleIndicator3
 
+class TutorialActivity : AppCompatActivity(){
+    private var titlesList= mutableListOf<String>()
+    private var descList= mutableListOf<String>()
+    private var imagesList= mutableListOf<Int>()
 
-
-
-
-class TutorialActivity : AppCompatActivity() {
-
-    private var titlesList = mutableListOf<String>()
-    private var descList = mutableListOf<String>()
-    private var imagesList = mutableListOf<Int>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tutorial)
 
         postToList()
-        val view_page2= findViewById<ViewPager2>(R.id.)
-        view_page2.adapter = ViewPagerAdapter(titlesList,descList,imagesList)
-        view_page2.orientation =ViewPager2.ORIENTATION_HORIZONTAL
+        val view_pager2= findViewById<ViewPager2>(R.id.view_pager2)
+        view_pager2.adapter= ViewPagerAdapter(titlesList, descList, imagesList)
+        view_pager2.orientation= ViewPager2.ORIENTATION_HORIZONTAL
 
-        val indicator = findViewById<CircleIndicator3>(R.id.indicator)
-        indicator.setViewPager(view_page2)
+        val indicator= findViewById<CircleIndicator3>(R.id.indicator)
+        indicator.setViewPager(view_pager2)
+
+        view_pager2.apply {
+            beginFakeDrag()
+            fakeDragBy(-10f)
+            endFakeDrag()
+        }
     }
-
-    private fun addToList(title: String, description: String, image: Int){
+    private fun addToList(title: String, description: String, image: Int) {
         titlesList.add(title)
         descList.add(description)
         imagesList.add(image)
     }
     private fun postToList(){
-        for(i in 1..5){
-            addToList("Title $i", "Description $i", R.mipmap-anydpi-v26.ic_launcher_round)
+        for(i in 1..3){
+            addToList("Title $i", "Description $i", R.mipmap.ic_launcher_round)
         }
-    }
+
+        }
 }
--- app/src/main/java/cat/urv/deim/asm/patinfly/views/tutorial/ViewPagerAdapter.kt
+++ app/src/main/java/cat/urv/deim/asm/patinfly/views/tutorial/ViewPagerAdapter.kt
@@ -1,6 +1,5 @@
 package cat.urv.deim.asm.patinfly.views.tutorial
 
-import android.os.Build.VERSION_CODES.R
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,7 +9,7 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import cat.urv.deim.asm.patinfly.R
 
-class ViewPagerAdapter(private var title: List<String>, private var details: List<String>, private var images: List<Int>): RecyclerView.Adapter<ViewPagerAdapter.Pager2ViewHolder>(){
+class ViewPagerAdapter (private var title: List<String>, private var details: List<String>, private var images: List<Int>) : RecyclerView.Adapter<ViewPagerAdapter.Pager2ViewHolder> (){
 
     inner class Pager2ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
@@ -19,26 +18,25 @@ class ViewPagerAdapter(private var title: List<String>, private var details: Lis
         val itemImage: ImageView = itemView.findViewById(R.id.ivImage)
 
         init{
-            itemImage.setOnClickListener{ v:View ->
-                val position = adapterPosition
-                Toast.makeText(itemView.context, "You clicked on item #${position +1}", Toast.LENGTH_SHORT).show()
-            }
+        itemImage.setOnClickListener{ v: View ->
+            val position = adapterPosition
+            Toast.makeText(itemView.context, "You clicked on item #${position + 1}", Toast.LENGTH_SHORT).show()
+        }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerAdapter.Pager2ViewHolder {
-        return Pager2ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_page, parent, false))
+        return Pager2ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_page,parent,false))
     }
 
-    override fun getItemCount(): Int {
-        return title.size
-    }
-
-    override fun onBindViewHolder(holder: Pager2ViewHolder, position: Int) {
-
+    override fun onBindViewHolder(holder: ViewPagerAdapter.Pager2ViewHolder, position: Int) {
         holder.itemTitle.text = title[position]
         holder.itemDetails.text = details[position]
         holder.itemImage.setImageResource(images[position])
+
     }
 
+    override fun getItemCount(): Int {
+       return title.size
+    }
 }
