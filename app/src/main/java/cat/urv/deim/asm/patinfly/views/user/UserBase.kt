24d64993d762954202e8c5d717ee3ff61003884f
claudiatombascoll <claudia.tombas@estudiants.urv.cat>
2023-03-27 12:51:10 +0200

    user

--- app/src/main/java/cat/urv/deim/asm/patinfly/views/user/UserBase.kt
+++ app/src/main/java/cat/urv/deim/asm/patinfly/views/user/UserBase.kt
@@ -1,13 +1,14 @@
-package cat.urv.deim.asm.patinfly.views.user;
+package cat.urv.deim.asm.patinfly.views.user
 
-    import java.util.regex.Pattern
-    class UserBase {
-        companion object{
-            public fun isInvalidUser(email: String): Boolean {
-                val usu = Pattern.compile(" ")
-                return usu.matcher(email).matches()
-            }
 
+import java.util.regex.Pattern
+import java.util.regex.Matcher
+open class UserBase {
+    companion object {
+        public fun isInvalidUser(email: String): Boolean {
+            val usu = Pattern.compile(" ")
+            return usu.matcher(email).matches()
         }
+
     }
 }
